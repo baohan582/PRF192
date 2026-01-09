330dd25c7752aa3996019cf55e6476b6966e1f6d
baohan582 <han041801@gmail.com>
2026-01-09 11:49:13 +0700

    baitap

--- slot2/bai3.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-	//Khai bao 1 bien số thực a, b, khởi tạo giá trị
-	
-	//Khai báo biến kq, là kết quả của a+b
-	//In a: a=5
-	//In b: b=10.216
-	//In kết quả: 5 + 10.216 = 15.22 
-}
